e0b754724b600b8c34eb0a8118bdc961b2e8f9dc
eplq <91148468+eplq@users.noreply.github.com>
2023-09-04 23:49:09 +0200

    INCOMPLETA: añadiendo las fuentes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -9,7 +11,10 @@ module.exports = {
 				accent: '#d41d6c',
 				background: '#f6f6f6',
 				text: '#151515'
-			}
+			},
+			fontFamily: {
+				sans: ['Assistant', 'sans-serif', ...defaultTheme.fontFamily.sans],
+			},
 		},
 	},
 	plugins: [],
